73d2096bbd0b59be011896f98150670fac2716f5
Dat Dinhquoc <datdinhquoc@abivin.com>
2019-05-12 00:03:05 +0000

    msg

--- t.py
+++ t.py
@@ -1,7 +1,51 @@
+"""
+\file
+\brief TensorFlow Raw library main class file
+"""
+
+#core
+import os;
+import pprint     as pp;
+import subprocess as sp;
+
+#libs
+import numpy      as np;
+import tensorflow as tf;
+
+#shortcut
+log = tf.logging.info;
+
+"""
+\brief TensorFlow Raw library main class
+"""
 class t:
 
-  def rm_model_dir():
-    print(999111);
+  """
+  \brief Pretty print
+  """
+  def pprint(Var):
+    log(pp.pformat(Var,indent=2));
+  #end def
+
+  """
+  \brief Halt the programme
+  """
+  def halt():
+    os._exit(1);
+  #end def
+
+  """
+  \brief Remove a TensorFlow model dir
+  """
+  def rm_model_dir(Model_Dir):
+    sp.check_output(["rm","-rf",Model_Dir]);
+  #end def
+
+  """
+  \brief Initialise TensorFlow
+  """
+  def init():
+    tf.logging.set_verbosity(tf.logging.INFO);
   #end def
 #end class
 
